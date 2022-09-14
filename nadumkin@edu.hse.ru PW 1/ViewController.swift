aeaf75e1790277db44ec38067a3ee858d360ca9c
ndumkin <79084750+ndumkin@users.noreply.github.com>
2022-09-14 23:25:59 +0300

    Initial Commit

---
+++ nadumkin@edu.hse.ru PW 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nadumkin@edu.hse.ru PW 1
+//
+//  Created by Никита Думкин on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
