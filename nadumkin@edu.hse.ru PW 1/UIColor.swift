9fbd4e00e7fa9e886700ba9f3b744e2852f9f4b2
ndumkin <79084750+ndumkin@users.noreply.github.com>
2022-09-16 01:09:55 +0300

    uploading

---
+++ nadumkin@edu.hse.ru PW 1/UIColor.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor.swift
+//  nadumkin@edu.hse.ru PW 1
+//
+//  Created by Никита Думкин on 15.09.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    convenience init(hex: Int, alpha: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat((hex >> 16) & 0xFF) / 255.0,
+            green: CGFloat((hex >> 8) & 0xFF) / 255.0,
+            blue: CGFloat(hex & 0xFF) / 255.0,
+            alpha: alpha
+        );
+    }
+}
--- nadumkin@edu.hse.ru PW 1/ViewController.swift
+++ nadumkin@edu.hse.ru PW 1/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let randomColorMax: Int = 0xFFFFFF;
+    let randomColorMin: Int = 0x000000;
+    
+//    enum Consts: CGFloat{
+//        case randomColorMax = 0xFFFFFF
+//        case randomColorMin = 0x0
+//    }
+    
+    @IBOutlet var views: [UIView]!
+    @IBOutlet weak var colorChangeButton: UIButton!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        super.viewDidLoad();
+        
+        viewChange(animationDuration: 2); // initing the start values for color and corner radius
     }
 
+    @IBAction func colorButtonPressed(_ sender: Any) {
+        
+        viewChange();
+    }
+    
+    func colorSetGenerator() -> Set<UIColor>{ // generating set of UIColor elements to except color repeating
+        var set = Set<UIColor>();
+        
+        while set.count < views.count{
+            set.insert(UIColor(hex: .random(in: randomColorMin...randomColorMax)));
+        }
+        
+        return set;
+    }
+    
+    func cornerRadiusGenerator(view: UIView) -> CGFloat{ // generates a random cornerRadius for view
+        
+        let maxCornerValue = (view.frame.size.height < view.frame.size.width ? view.frame.size.height : view.frame.size.width) / 2; // compare the width and height to find out the maximum for cornerRadius and then value is divisioned by 2 for better appearance
+        
+        return .random(in: 0...maxCornerValue); // generating a random corneradius value
+        
+    }
+    
+    func viewChange(animationDuration: TimeInterval = 1){
+        
+        var set = colorSetGenerator();
+        
+        colorChangeButton.isEnabled = false;
+        
+        for view in views{
+            UIView.animate(
+                withDuration: animationDuration,
+                animations:
+                    {
+                        view.backgroundColor = set.popFirst();
+                        view.layer.cornerRadius = self.cornerRadiusGenerator(view: view);
+                    },
+                completion:
+                    {
+                        (finished: Bool) in
+                        self.colorChangeButton.isEnabled = true;
+                    }
 
+            );
+        }
+    }
+    
 }
 
